d07c265f9f0516e7bea8d775931d22f0e91f12cb
ilanchezhian <ilanchezhiancse@gmail.com>
2019-01-04 22:20:02 +0530

    first

---
+++ src/main/java/com/models/DataModel.java
@@ -0,0 +1,32 @@
+package com.models;
+
+
+public class DataModel {
+    private Integer id;
+    private String name;
+    private Double average;
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public Double getAverage() {
+        return average;
+    }
+
+    public void setAverage(Double average) {
+        this.average = average;
+    }
+}
